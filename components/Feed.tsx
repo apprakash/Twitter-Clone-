4d614efeeb898c4d1e84486a9a498bbb6ac3dbce
apprakash <jimpoorna@gmail.com>
2022-08-08 19:32:04 +0530

    Responsive CSS Grid

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,17 +3,23 @@ import React from 'react'
 
 function Feed() {
   return (
-    <div>
-        <div className='flex items-center justify-between'>
-            <h1 className='p-5 pb-0 text-xl font-bold'>
-                Home
-            </h1>
-            <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter
-            mr-5 mt-5 transition-all duration-500 ease-out
-            hover:rotate-180 active:scale-125' />
-        </div>
+    <div className='col-span-7 lg:col-span-5 border-x'>
+      <div className='flex items-center justify-between'>
+        <h1 className='p-5 pb-0 text-xl font-bold'>
+          Home
+        </h1>
+        <RefreshIcon className='h-8 w-8 cursor-pointer text-twitter
+          mr-5 mt-5 transition-all duration-500 ease-out
+          hover:rotate-180 active:scale-125' 
+        />
+      </div>
+
+
+
+
+
     </div>
   )
-}
+} 
 
 export default Feed
