16a1e826a7727dcd2f7343861c717763b0e7940a
apprakash <jimpoorna@gmail.com>
2022-08-10 00:41:18 +0530

    Main feed

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import { Tweet } from '../typing'
+import TimeAgo from 'react-timeago'
+import { 
+    ChatAlt2Icon,
+    HeartIcon,
+    SwitchHorizontalIcon,
+    UploadIcon
+} from '@heroicons/react/outline'
 
 interface Props{
     tweet: Tweet
@@ -7,7 +14,54 @@ interface Props{
 
 function Tweet( { tweet}: Props) {
   return (
-    <div>Tweet</div>
+    <div className='flex flex-col space-x-3 border-y border-gray-100 p-5'>
+        <div className=' flex space-x-3'>
+            <img 
+                className='h-10 w-10 rounded-full object-cover'
+                src={tweet.profileImg} alt="" 
+            />
+
+            <div>
+                <div className='flex items-center space-x-1'>
+                    <p className='mr-1 font-bold'> 
+                        {tweet.username } 
+                    </p>
+                    <p className='hidden text-sm text-gray-500 sm:inline'>
+                        @{tweet.username.replace(/\s+/g, '').toLowerCase()} .
+                    </p>
+                    <TimeAgo
+                        className='text-sm text-gray-500'
+                        date={tweet._createdAt}
+                    />
+                </div>
+                
+                <p className='pt-1'>
+                    {tweet.text}
+                    {tweet.image && 
+                     <img 
+                        className='m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm'
+                        src={tweet.image} alt="" 
+                     />
+                    }
+                </p>
+            </div>
+        </div>
+        <div className='mt-5 flex justify-between'>
+            <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
+                <ChatAlt2Icon className='h-5 w-5' />
+                <p>3</p>
+            </div>
+            <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
+                <SwitchHorizontalIcon className='h-5 w-5' />
+            </div>
+            <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
+                <HeartIcon className='h-5 w-5' />
+            </div>
+            <div className='flex cursor-pointer items-center space-x-3 text-gray-400'>
+                <UploadIcon className='h-5 w-5' />
+            </div>
+        </div>
+    </div>
   )
 }
 
