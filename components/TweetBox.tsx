e8730b23f735e5486674eeef614ce793078b048e
apprakash <jimpoorna@gmail.com>
2022-08-11 19:31:39 +0530

    Image Func

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import {
     CalendarIcon,
     EmojiHappyIcon,
@@ -12,50 +12,88 @@ import { useSession } from 'next-auth/react'
 function TweetBox() {
 
     const [input, setInput] = useState<string>('')
+    const [image, setImage] = useState<string>('')
+
+    const imageInputRef = useRef<HTMLInputElement>(null)
+
     const { data : session } = useSession()
+    const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false)
+
+    const addImageToTweet = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
+
+        if(!imageInputRef.current?.value) return;
+        setImage(imageInputRef.current.value)
+        imageInputRef.current.value = '';
+        setImageUrlBoxIsOpen(false); 
+    }
     
-  return (
-    <div className='flex space-x-2 p-5'>
-        <img 
-            className='h-14 w-14 rounded-full object-cover mt-4'
-            src={session?.user?.image || "https://links.papareact.com/gll" }
-            alt=""
-        />
+    return (
+        <div className='flex space-x-2 p-5'>
+            <img 
+                className='h-14 w-14 rounded-full object-cover mt-4'
+                src={session?.user?.image || "https://links.papareact.com/gll" }
+                alt=""
+            />
 
-        <div className='flex flex-1 items-center pl-2'>
-            <form className='flex flex-1 flex-col'>
-                <input 
-                    value={input}
-                    onChange={(e) => setInput(e.target.value)}
-                    className='h-24 w-full text-xl outline-none placeholder:text-xl'
-                    type="text" placeholder='Tweet here!' 
-                />
+            <div className='flex flex-1 items-center pl-2'>
+                <form className='flex flex-1 flex-col'>
+                    <input 
+                        value={input}
+                        onChange={(e) => setInput(e.target.value)}
+                        className='h-24 w-full text-xl outline-none placeholder:text-xl'
+                        type="text" placeholder='Tweet here!' 
+                    />
 
-                <div className='flex items-center'>
-                    <div className='flex flex-1 space-x-2 text-twitter'>
-                        <PhotographIcon className='h-5 w-5 cursor-pointer transition-transform
-                            duration-150 ease-out hover:scale-150' 
-                        />
-                        <SearchCircleIcon className='h-5 w-5' />
-                        <EmojiHappyIcon className='h-5 w-5' />
-                        <CalendarIcon className='h-5 w-5' />
-                        <LocationMarkerIcon className='h-5 w-5' />                
+                    <div className='flex items-center'>
+                        <div className='flex flex-1 space-x-2 text-twitter'>
+                            <PhotographIcon 
+                                onClick={() => setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)}
+                                className='h-5 w-5 cursor-pointer transition-transform
+                                duration-150 ease-out hover:scale-150' 
+                            />
+                            <SearchCircleIcon className='h-5 w-5' />
+                            <EmojiHappyIcon className='h-5 w-5' />
+                            <CalendarIcon className='h-5 w-5' />
+                            <LocationMarkerIcon className='h-5 w-5' />                
+                        </div>
+
+                        <button 
+                            disabled={!input || !session}
+                            className='bg-twitter px-5 py-2 font-bold text-white 
+                            rounded-full disabled:opacity-40'
+                        >
+                            Tweet
+                        </button>
                     </div>
 
-                    <button 
-                        disabled={!input || !session}
-                        className='bg-twitter px-5 py-2 font-bold text-white 
-                        rounded-full disabled:opacity-40'
-                    >
-                        Tweet
-                    </button>
+                    {imageUrlBoxIsOpen && (
+                        <form className='mt-5 flex py-2 px-4 rounded-lg bg-twitter/80'>
+                            <input 
+                                ref = {imageInputRef}
+                                className='flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white'
+                                placeholder='Enter Image URL...'
+                                type="text" 
+                            />
+                            <button 
+                                type="submit" onClick={e => addImageToTweet}
+                                className='font-bold text-white'>
+                                Add Image 
+                            </button>
+                        </form>
+                    )}
 
-                </div>
-
-            </form>
+                    {image && (
+                        <img 
+                            className='mt-10 h-40 w-full rounded-xl object-contain shadow-xl'
+                            src={image} alt=""
+                        />
+                    )}
+                    
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
  
 export default TweetBox  
